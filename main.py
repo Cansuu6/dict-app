3dedf89f31756d335ffbd479bc5eb5e4024cae38
Cansuu6 <168909004+Cansuu6@users.noreply.github.com>
2024-05-18 13:14:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,29 @@ meme_dict = {
             "LOL": "Komik bir şeye verilen cevap",
             "ROFL" : "bir şakaya karşılık cevap",
             "SHEESH" : "onaylamamak",
-            "CREEPY" : "korkunç",
-            "AGGRO" : "agresifleşmek/sinirlenmek"
+            "CREEPY" : "korkunç"
             }
             
-print("Anlamlarını öğrenebileceğiniz kelimeler:", *meme_dict.keys())
+meme1_dict = {
+            "GOAT": "Tüm zamanlarin en iyisi",
+            "YOLO": "bir kez hayata gelirsin",
+            "AGGRO" : "agresifleşmek/sinirlenmek"
+             }
+            
+print("Anlamlarını öğrenebileceğiniz kelimeler:", *meme_dict.keys(), *meme1_dict.keys())
 
 
-for i in range(5):
+for i in range(7):
     x = input("Hangi kelimenin anlamanı öğrenmek istersiniz?")
     x = x.upper()
     
     if x in meme_dict.keys():
         # Kelime eşleşiyorsa ne yapmalıyız?
         print(x, "kelimesinin anlamı: ", meme_dict[x])
+        
+    elif x in meme1_dict.keys():
+        # Kelime eşleşiyorsa ne yapmalıyız?
+        print(x, "kelimesinin anlamı: ", meme1_dict[x])
     else:
         # Kelime eşleşmiyorsa ne yapmalıyız?
         print("Maalesef yazdığınız kelime sözlükte bulunmamaktadır!")
